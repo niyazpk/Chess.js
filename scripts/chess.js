de2a4db00790de3441e7c542086ac8c0484d878f
Niyaz PK <niyazpk@gmail.com>
2012-01-02 23:46:07 +0530

    Changed piece values / Drag-drop added / Validation stub

--- scripts/chess.js
+++ scripts/chess.js
@@ -9,19 +9,22 @@ window.log = function() {
     }
 }
 
-var WHITE_KING = 100;
-var WHITE_QUEEN = 90;
-var WHITE_ROOK = 50;
-var WHITE_BISHOP = 31;
-var WHITE_KNIGHT = 30;
-var WHITE_PAWN = 10;
+var WHITE_PAWN = 0x01;
+var WHITE_KNIGHT = 0x02;
+var WHITE_KING = 0x03;
+var WHITE_BISHOP = 0x05;
+var WHITE_ROOK = 0x06;
+var WHITE_QUEEN = 0x07;
 
-var BLACK_KING = -WHITE_KING;
-var BLACK_QUEEN = -WHITE_QUEEN;
-var BLACK_ROOK = -WHITE_ROOK;
-var BLACK_BISHOP = -WHITE_BISHOP;
-var BLACK_KNIGHT = -WHITE_KNIGHT;
-var BLACK_PAWN = -WHITE_PAWN;
+var BLACK_PAWN = 0x09;
+var BLACK_KNIGHT = 0x0A;
+var BLACK_KING = 0x0B;
+var BLACK_BISHOP = 0x0D;
+var BLACK_ROOK = 0x0E;
+var BLACK_QUEEN = 0x0F;
+
+
+var currentPlayer = 1;  // whose turn is it now?    1 = white, -1 = black
 
 var board = [[BLACK_ROOK, BLACK_KNIGHT, BLACK_BISHOP, BLACK_QUEEN, BLACK_KING, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK],
              [BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN, BLACK_PAWN],
@@ -35,55 +38,43 @@ var board = [[BLACK_ROOK, BLACK_KNIGHT, BLACK_BISHOP, BLACK_QUEEN, BLACK_KING, B
 
 function getPieceName(pieceValue){
     switch (pieceValue) {
-        case WHITE_KING:
-            return 'WHITE_KING';
-            break;
-        case WHITE_QUEEN:
-            return 'WHITE_QUEEN';
-            break;
-        case WHITE_ROOK:
-            return 'WHITE_ROOK';
-            break;
-        case WHITE_BISHOP:
-            return 'WHITE_BISHOP';
-            break;
-        case WHITE_KNIGHT:
-            return 'WHITE_KNIGHT';
-            break;
-        case WHITE_PAWN:
-            return 'WHITE_PAWN';
-            break;
+        case WHITE_KING: return 'WHITE_KING';
+        case WHITE_QUEEN: return 'WHITE_QUEEN';
+        case WHITE_ROOK: return 'WHITE_ROOK';
+        case WHITE_BISHOP: return 'WHITE_BISHOP';
+        case WHITE_KNIGHT: return 'WHITE_KNIGHT';
+        case WHITE_PAWN: return 'WHITE_PAWN';
         
-        case BLACK_KING:
-            return 'BLACK_KING';
-            break;
-        case BLACK_QUEEN:
-            return 'BLACK_QUEEN';
-            break;
-        case BLACK_ROOK:
-            return 'BLACK_ROOK';
-            break;
-        case BLACK_BISHOP:
-            return 'BLACK_BISHOP';
-            break;
-        case BLACK_KNIGHT:
-            return 'BLACK_KNIGHT';
-            break;
-        case BLACK_PAWN:
-            return 'BLACK_PAWN';
-            break;
+        case BLACK_KING: return 'BLACK_KING';
+        case BLACK_QUEEN: return 'BLACK_QUEEN';
+        case BLACK_ROOK: return 'BLACK_ROOK';
+        case BLACK_BISHOP: return 'BLACK_BISHOP';
+        case BLACK_KNIGHT: return 'BLACK_KNIGHT';
+        case BLACK_PAWN: return 'BLACK_PAWN';
         
-        default:
-            return 'EMPTY';
-            break;
+        default: return 'EMPTY';
     }
 }
 
+var onDrop = function(event, ui) {
+            
+    var parent = ui.draggable.parent();
+    
+    if(validateMove(currentPlayer, [parent.data('row'), parent.data('column')], [$(this).data('row'), $(this).data('column')])){
+        
+        board[$(this).data('row')][$(this).data('column')] = board[parent.data('row')][parent.data('column')];
+        board[parent.data('row')][parent.data('column')] = 0;
+        currentPlayer = -currentPlayer;
+        drawBoard(board);
+        
+    }else{
+        log('invalid move');
+        drawBoard(board);
+    }
+}
 
 $(function(){
-
     drawBoard(board);
-    
 });
 
 function drawBoard(board){
@@ -92,11 +83,58 @@ function drawBoard(board){
         str += '<div class="row">';
         for( var j = 0 ; j < 8 ; j++ ){
             str += '<div class="column ' +
-            ( (i + j) % 2 === 0 ? 'light': 'dark') + '">' +
+            ( (i + j) % 2 === 0 ? 'light': 'dark') +
+            '" data-row="' + i + '" data-column="' + j + '">' +
             '<div class="' + getPieceName(board[i][j]) + '"></div>' +
             '</div>';
         }
         str += '</div>';
     }
-    $('#board').append(str);
+    $('#board').html(str);
+    
+    $( ".column" ).droppable({
+        drop: onDrop
+    });
+    
+    $( ".column div" ).draggable({ revert: 'invalid' });
+}
+
+function validateMove(currentPlayer, from, to){
+    
+    if(currentPlayer * board[from[0]][from[1]] <= 0) {  // not your turn? 0 = Moving from an empty square
+        return false;
+    }
+    
+    if(currentPlayer * board[to[0]][to[1]] > 0) {  // cannot attack one of your own - empty move is also convered here
+        return false;
+    }
+    
+    var dY = from[0] - to[0];
+    var dX = from[1] - to[1];
+    var dXY = dX * dY;
+    var dYabs = Math.abs(dY);
+    var dXabs = Math.abs(dY);
+        
+    // rook - any rook
+    if(currentPlayer * board[from[0]][from[1]] === WHITE_ROOK){
+        if( dXY !== 0 ){  // rook can only move in a file or a rank
+            return false;
+        }
+    }
+    
+    // bishop - any bishop
+    if(currentPlayer * board[from[0]][from[1]] === WHITE_BISHOP){
+        if( dXabs !== dYabs ){  // bishop can only move diagonally
+            return false;
+        }
+    }
+
+    // king - any king
+    if(currentPlayer * board[from[0]][from[1]] === WHITE_KING){
+        //if(  ){
+            return false;
+        //}
+    }
+    
+    return true;
 }
